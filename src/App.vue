f6e7cd5058c775e2867895a93a30dc62edad2201
DaRaForU <san.sovandara.fe@gmail.com>
2023-10-02 22:07:28 +0700

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+  import {ref} from 'vue';
+  const count = ref(0);
+
+  const addToCount = () => {
+    count.value += 1;
+  };
+
+  const subStractToCount = () => {
+    count.value -= 1;
+  };
+
+  console.log(typeof count)
+</script>
+
+<template>
+  <main>
+    <div>
+      <h1>The Current...</h1>
+      <h1>{{ count }}</h1>
+      <button @click="addToCount()">+</button>
+      <button @click="subStractToCount()">-</button>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+  main{
+    width: 100wh;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  div{
+    text-align: center;
+  }
+</style>
